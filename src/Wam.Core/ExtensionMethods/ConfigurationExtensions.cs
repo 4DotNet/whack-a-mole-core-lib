f58bc13e86cb3b5b5eb5af4171ea72c87e59ac33
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-01-16 14:40:43 +0100

    Adding GetRequiredValue from Config

---
+++ src/Wam.Core/ExtensionMethods/ConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Wam.Core.ExtensionMethods;
+
+public static class ConfigurationExtensions
+{
+    public static string GetRequiredValue(this IConfiguration configuration, string key, string? description = null)
+    {
+        string value = configuration.GetValue<string>(key);
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new InvalidOperationException("Missing setting " + ((description != null) ? ("for " + description) : "") + " : " + key);
+        }
+
+        return value;
+    }
+}
