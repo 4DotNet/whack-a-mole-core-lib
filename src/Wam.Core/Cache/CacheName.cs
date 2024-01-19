283cd167b45fe91ca516c352864e53ffef10b116
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-01-19 18:09:56 +0100

    Adding central generation of cache keys

---
+++ src/Wam.Core/Cache/CacheName.cs
@@ -0,0 +1,9 @@
+﻿namespace Wam.Core.Cache;
+
+public static class CacheName
+{
+    public static string UserDetails(Guid userId) => $"UserDetails:{userId:N}";
+
+    public static string GameDetails(Guid userId) => $"GameDetails:Id:{userId:N}";
+    public static string GameDetails(string gameCode) => $"GameDetails:Code:{gameCode}";
+}
