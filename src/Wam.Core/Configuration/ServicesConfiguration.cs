f18ea3c7835a07a0f642b253074c0fdd74bb18d7
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-01-22 15:54:53 +0100

    Realtime service

--- src/Wam.Core/Configuration/ServicesConfiguration.cs
+++ src/Wam.Core/Configuration/ServicesConfiguration.cs
@@ -4,8 +4,9 @@ public class ServicesConfiguration
 {
     public const string SectionName = "Services";
 
-    public string GamesService { get; set; }
-    public string UsersService { get; set; }
-    public string ScoresService { get; set; }
-    public string VouchersService { get; set; }
+    public string? GamesService { get; set; }
+    public string? UsersService { get; set; }
+    public string? ScoresService { get; set; }
+    public string? VouchersService { get; set; }
+    public string? RealtimeService { get; set; }
 }
