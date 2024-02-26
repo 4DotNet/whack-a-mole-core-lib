a4a5e977edd9abc0eead6dcdadbbc523de6fa05a
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-02-26 12:34:01 +0100

    Updating the package to contain infra

---
+++ src/Wam.Core/Authentication/Swagger/ConfigureSwaggerUIOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Wam.Core.Authentication.Swagger;
+
+public class ConfigureSwaggerUIOptions : IConfigureNamedOptions<SwaggerUIOptions>
+{
+    private readonly AuthenticationSettings _authenticationSettings;
+
+    public ConfigureSwaggerUIOptions(IOptions<AuthenticationSettings> authenticationSettings)
+    {
+        _authenticationSettings = authenticationSettings.Value;
+    }
+
+    public void Configure(string name, SwaggerUIOptions options)
+    {
+        if (_authenticationSettings.ClientId != Guid.Empty)
+        {
+            options.OAuthClientId(_authenticationSettings.ClientId.ToString("D"));
+        }
+    }
+
+    public void Configure(SwaggerUIOptions options)
+    {
+        Configure(Options.DefaultName, options);
+    }
+}
---
+++ src/Wam.Core/Authentication/Swagger/SwaggerApiOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Wam.Core.Authentication.Swagger;
+
+internal class SwaggerApiOptions
+{
+    public string Title { get; set; } = "";
+    public string? Description { get; set; }
+}
