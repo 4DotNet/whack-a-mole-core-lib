3284072cc6aad63ec490082abbe983a446bf42cd
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-09-23 16:38:46 +0200

    Adding central cache name for voucher information

--- src/Wam.Core/Cache/CacheName.cs
+++ src/Wam.Core/Cache/CacheName.cs
@@ -3,9 +3,8 @@
 public static class CacheName
 {
     public static string UserDetails(Guid userId) => $"wam:user:id:{userId:N}";
-
     public static string GameDetails(Guid gameId) => $"wam:game:id:{gameId:N}";
     public static string GameDetails(string gameCode) => $"wam:game:code:{gameCode}";
-
     public static string GameScoreBoard(Guid gameId) => $"wam:game:scoreboard:{gameId:N}";
+    public static string VoucherInfo(Guid voucherId) => $"wam:vouchers:{voucherId:N}";
 }
