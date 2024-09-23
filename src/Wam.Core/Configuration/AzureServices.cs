7c24dbe4fcf56585a4f4a154829debcbd1193f43
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-09-23 16:42:54 +0200

    Updating pipeline versions

--- src/Wam.Core/Configuration/AzureServices.cs
+++ src/Wam.Core/Configuration/AzureServices.cs
@@ -3,10 +3,10 @@
 public class AzureServices
 {
     public const string SectionName = "AzureServices";
-    public string WebPubSubEndpoint { get; set; }
-    public string WebPubSubHub{ get; set; }
-    public string UsersStorageAccountName { get; set; }
-    public string GamesStorageAccountName { get; set; }
-    public string ScoresStorageAccountName { get; set; }
-    public string VouchersStorageAccountName { get; set; }
+    public string WebPubSubEndpoint { get; set; } = null!;
+    public string WebPubSubHub{ get; set; } = null!;
+    public string UsersStorageAccountName { get; set; } = null!;
+    public string GamesStorageAccountName { get; set; } = null!;
+    public string ScoresStorageAccountName { get; set; } = null!;
+    public string VouchersStorageAccountName { get; set; } = null!;
 }
