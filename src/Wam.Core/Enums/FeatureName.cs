303ca9adddd125d96ec2253a2089ea68fce182ee
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-09-24 13:48:34 +0200

    +semver: minor - Adding central feature names

---
+++ src/Wam.Core/Enums/FeatureName.cs
@@ -0,0 +1,7 @@
+﻿namespace Wam.Core.Enums;
+
+public class FeatureName
+{
+    public const string EnableMaxPlayersFeature = "enable-max-players";
+    public const string EnableVouchersFeature = "enable-vouchers";
+}
